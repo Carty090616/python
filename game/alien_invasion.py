89d2b1c2b89a3be04875d39a5d5e098c2396cff0
carty <1032188134@qq.com>
2019-01-26 16:20:12 +0800

    修改

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -1,7 +1,12 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 import game_functions as gf
 
 def run_game():
@@ -11,16 +16,33 @@ def run_game():
     screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 创建飞船
+    # 创建存储游戏统计信息的实例，并创建记分牌
+    stats = GameStats(ai_setting)
+    sb = Scoreboard(ai_setting, screen, stats)
+
+    # 创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_setting, screen)
+    alien = Alien(ai_setting, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_setting, screen, ship, aliens)
+
+    # 创建Play按钮
+    play_button = Button(ai_setting, screen, "Play")
 
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.checkz_events(ship)
-        ship.update()
-        # 每次循环时都重新绘制屏幕
-        # 让最近绘制的屏幕可见
-        gf.update_screen(ai_setting, screen, ship)
+        gf.check_events(ai_setting, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            # 每次循环时都重新绘制屏幕
+            # 让最近绘制的屏幕可见
+            gf.update_bullets(ai_setting, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_setting, screen, stats, sb, ship, aliens, bullets)
+
+        gf.update_screen(ai_setting, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
