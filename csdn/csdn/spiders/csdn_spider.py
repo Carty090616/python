8997fb67d3015b1fb3748a1280bb97816596fbbc
lifeifei <1032188134@qq.com>
2019-02-21 10:43:39 +0800

    添加csdn

---
+++ csdn/csdn/spiders/csdn_spider.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from csdn.items import CsdnItem
+
+
+class CsdnSpiderSpider(scrapy.Spider):
+    name = 'csdn_spider'
+    allowed_domains = ['csdn.net']
+    start_urls = ['http://blog.csdn.net/nav/news']
+
+    def parse(self, response):
+        # 先获取页面中class="list_con"的div标签，在获取class="title oneline"的子div标签
+        body = response.xpath('//div[@class="list_con"]//div[@class="title oneline"]')
+        for value in body:
+            item = CsdnItem()
+            try:
+                item['title'] = value.xpath('./h2/a/text()')[0].extract().strip()
+                item['href'] = value.xpath('./h2/a/@href')[0].extract()
+            except Exception as e:
+                print(e)
+            else:
+                print(item['title'] + ':' + item['href'])
+            yield item
